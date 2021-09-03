53c063b83b678f7cf96006afaaff7dfc04c31a37
Dzmitry Naumau <d.naumau@godeltech.com>
2021-09-03 17:20:40 +0200

    demo

--- fastfill.js
+++ fastfill.js
@@ -1,6 +1,6 @@
 (async function run(){
     var el = document.querySelector('body');
     const response = await fetch('/index.html');
-    const html = response.text();
+    const html = await response.text();
     el.innerHTML += html;
 })();
