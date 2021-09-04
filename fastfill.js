21a9605887227bcc76f0a031289c3c7fc702c22b
Dzmitry Naumau <d.naumau@godeltech.com>
2021-09-04 07:42:28 +0200

    demo

--- fastfill.js
+++ fastfill.js
@@ -1,6 +1,6 @@
 (async function run(){
     var el = document.querySelector('body');
-    const response = await fetch('/index.html');
+    const response = await fetch('https://fastfill.netlify.app/index.html');
     const html = await response.text();
     el.innerHTML += html;
 })();
