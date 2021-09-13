aee30b987c329bf3b2c5b604947c77fdc39cf729
Dzmitry Naumau <d.naumau@godeltech.com>
2021-09-13 17:06:18 +0200

    add new

--- fastfill.js
+++ fastfill.js
@@ -1,6 +1,10 @@
 (async function run(){
     var el = document.querySelector('body');
+    
     const response = await fetch('https://fastfill.netlify.app/index.html');
     const html = await response.text();
     el.insertAdjacentHTML("afterbegin", html);
+
+    var fastfill = document.querySelector('#fastfill');
+    fastfill.scrollIntoView({block: "start", behavior: "smooth"});
 })();
