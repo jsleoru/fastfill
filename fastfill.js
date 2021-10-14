71f2cda0fb1d1421f37d3a25877fe6ad3a0510fc
Dzmitry Naumau <d.naumau@godeltech.com>
2021-10-14 14:50:34 +0200

    fsg

--- fastfill.js
+++ fastfill.js
@@ -74,6 +74,8 @@
                      "risk": { "amount": 1000, "term": 13, "title": { "dataCode": "MR", "displayText": "Mr" }, "firstName": "dsfga", "lastName": "sdfg", "depositAmount": 1021, "emailAddress": "ff@emailreaction.com", "annualIncomeGross": 4567, "purpose": { "dataCode": "C", "displayText": "Buying a Car" }, "residentialStatus": { "dataCode": "HOWM", "displayText": "Homeowner with mortgage" }, "employmentStatus": { "dataCode": "H", "displayText": "House Person" }, "dateOfBirth": "1995-03-15", "currentAddress": { "completeAddress": { "postOfficeAddress": { "organisationName": "B G L Group Ltd", "department": "", "subBuilding": "", "building": "", "number": "1", "dependentThoroughfare": "B G L Park", "thoroughfare": "Bakewell Road", "doubleDependentLocality": "", "dependentLocality": "Orton Southgate", "town": "Peterborough", "postalCounty": "", "postcode": "PE2 6XJ", "abbreviatedPostalCounty": "", "optionalCounty": "Cambridgeshire", "abbreviatedOptionalCounty": "Cambs", "traditionalCounty": "", "administrativeCounty": "Peterborough", "dps": "1A" }, "formattedAddress": { "line1": "B G L Group Ltd", "line2": "1 B G L Park", "line3": "Bakewell Road", "line4": "Orton Southgate", "line5": "Peterborough", "postcode": "PE2 6XJ" } }, "whenMovedDate": "2018-03-01" }, "dependents": 0, "childCarePaymentsMonthly": 0, "accommodationExpenditureMonthly": 543 }
                 }),
             })
+            
+        document.location.reload();
         window.open(`https://gateway.comparethemarket.com/loans/5.0/viewprices/wip/${wipID}?journey=https://loans-eligibility-enquiry.comparethemarket.com`,"_self")
     })
 })();
